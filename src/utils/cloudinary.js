912a9d2a77808bc9bec5821f671936208949c262
Rishika Pandey <rishikapandey02@gmail.com>
2025-11-20 03:10:32 +0530

    Add Register Controllers and bug Fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs"
 
-import { v2 as cloudinary } from 'cloudinary';
 
   // Configuration
 cloudinary.config({ 
@@ -15,13 +14,16 @@ const uploadOnCloudinary = async (localFilePath) =>{
 
   try {
     if(!localFilePath) return null
+    // upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type:"auto"
     })
+    // file has been uploaded successfull
     console.log("file is uploaded on cloudinary" , response.url);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath)
+    fs.unlinkSync(localFilePath) 
+    //remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 }
